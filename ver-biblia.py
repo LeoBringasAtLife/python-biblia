47bc8dfa915fb8607631c645b8a57f95c6562d62
LeoBringasAtLife <leonardoisaiasbringas34@gmail.com>
2025-11-06 21:27:00 +0100

    first commit

---
+++ ver-biblia.py
@@ -0,0 +1,29 @@
+
+# Two input fields: one to find the name and another to redirect to custom text (I think I've explained it correctly, haha)
+nombre = input("ingresa tu nombre: ").strip()
+genero = input("¿sos hombre o mujer? (h/m): ").strip().lower()
+
+
+if genero == 'm':
+    print(f"\n  'Amada, {nombre} una cosa es necesaria:")
+    print(f" Estar en mi presencia. Estar en mi presencia.")
+    print(f" Todo lo que está pasando en la tierra, todo es pasajero, pero mi presencia nadie te la va a quitar.")
+    print(f" Mi paz nadie te la va a quitar. Mi gozo nadie te lo va a quitar. Vení a mi presencia.")
+
+else:
+    print(f"\n  'Amado, {nombre} una cosa es necesario:'")
+    print(f" Estar en mi presencia, estar en mi presencia.")
+    print(f" Todo lo que está pasando en la tierra, todo es pasajero, pero mi presencia nadie te la va a quitar.")
+    print(f" Mi paz nadie te la va a quitar. Mi gozo nadie te lo va a quitar. Vení a mi presencia.")
+
+print(f"\n Lucas 10:41-42")
+
+
+
+
+#“my beloved, [your name], only one thing is necessary:
+
+#to be in my presence. To be in my presence.
+#everything that is happening on earth is temporary, but no one can take my presence from you.
+#no one can take my peace from you. No one can take my joy from you. Come into my presence,” the Lord called to me.
+''''''
